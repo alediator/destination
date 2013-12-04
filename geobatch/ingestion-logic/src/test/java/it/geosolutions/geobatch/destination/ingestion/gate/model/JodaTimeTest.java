d7066edb3e2e4237ebea7dc5061360b147f3541d
alediator <alejandro.diaz@geo-solutions.it>
2013-12-04 17:03:39 +0100

    Gate transit xml mapping
    Mapping for xml files for gate ingestion action.

---
+++ geobatch/ingestion-logic/src/test/java/it/geosolutions/geobatch/destination/ingestion/gate/model/JodaTimeTest.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (C) 2007 - 2013 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ * 
+ *  GPLv3 + Classpath exception
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.geosolutions.geobatch.destination.ingestion.gate.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Test;
+
+/**
+ * Test Joda time formatter
+ * 
+ * @author adiaz
+ */
+public class JodaTimeTest {
+
+private static final DateTimeFormatter FORMATTER = org.joda.time.format.DateTimeFormat
+        .forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").withZoneUTC();
+
+/**
+ * Test if the formatter run correctly between String and milliseconds
+ */
+@Test
+public void testJodaTime() {
+    String stringDate = "2013-10-23T10:34:00Z";
+    long millis = FORMATTER.parseMillis(stringDate);
+    assertEquals(stringDate, FORMATTER.print(millis));
+}
+
+}
